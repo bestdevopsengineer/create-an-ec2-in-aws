dd5145608ac5c6f5baa648b70f1eed2e7188c5a2
aristide dubois jou <bestdevopsengineer@gmail.com>
2026-02-13 16:50:33 -0600

    Update main.tf

--- operations/terraform/eks/main.tf
+++ operations/terraform/eks/main.tf
@@ -1,1 +1,28 @@
 
+terraform {
+  required_version = ">= 1.0.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.15.3"
+
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+
+  vpc_id     = var.vpc_id
+  subnet_ids = var.private_subnets
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = [var.instance_type]
+      min_size       = var.min_size
+      max_size       = var.max_size
+      desired_size   = var.desired_capacity
+    }
+  }
+}
